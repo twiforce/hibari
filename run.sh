fdd29f76983edf3d4eeda9f17477a496d30f00f5
Simon Twiforce <twiforce@yandex.ru>
2016-11-18 23:19:19 +0300

    run.sh update

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+PATH='/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin'
 
-while :
-do
-    node index.js
-    sleep 2
-
-done
+n=`ps auxwww|grep "node index.js"|grep -v grep|wc -l`
+if [ $n -eq 0 ]
+then
+	cd /home/synch/synchtube.ru
+	node index.js
+	echo `date` >> fuckyou.log
+fi
